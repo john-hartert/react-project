dee5a0452a8464bec4cf60afc7898d10812be98d
John Hartert <j.blainehartert@gmail.com>
2017-09-01 11:48:18 -0400

    Working message board

--- src/App.js
+++ src/App.js
@@ -4,14 +4,49 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.addPost = this.addPost.bind(this);
+    this.handlePostEditorInputChange = this.handlePostEditorInputChange.bind(this);
+    
+    this.state = {
+      posts: [],
+      newPostBody: '',
+    }
+  }
+
+
+addPost() {
+  const newState = Object.assign([], this.state);
+  newState.posts.push(this.state.newPostBody);
+  newState.newPostBody = '';
+  this.setState(newState);
+
+}
+
+handlePostEditorInputChange(ev) {
+  this.setState({
+    newPostBody: ev.target.value
+  })
+}
+
+
   render() {
     return (
       <div>
-        <Post />
+        { 
+          this.state.posts.map( (postBody, idx) => {
+            return (
+              <Post key={idx} postBody={postBody}/>
+              )
+          })
+        }
+        
         <div className="panel panel-default post-editor">
           <div className="panel-body">
-            <textarea className="form-control post-editor-input" />
-            <button className="btn btn-success post-editor-button">Post</button>
+            <textarea className="form-control post-editor-input" onChange={this.handlePostEditorInputChange}/>
+            <button className="btn btn-success post-editor-button" onClick={this.addPost}>Post</button>
           </div>
         </div>
       </div>
--- src/Post/component/Post.jsx
+++ src/Post/component/Post.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Post = (props) => (
     <div className="panel panel-default post-body">
           <div className="panel-body">
-            Here is the input
+            { props.postBody }
           </div>
         </div>
 );
