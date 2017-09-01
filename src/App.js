7214177607746862a0ce17890b8ded15267d9017
John Hartert <j.blainehartert@gmail.com>
2017-09-01 13:10:46 -0400

    Adds functional input output for messages. Also gets resets input after each onClick

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ handlePostEditorInputChange(ev) {
         
         <div className="panel panel-default post-editor">
           <div className="panel-body">
-            <textarea className="form-control post-editor-input" onChange={this.handlePostEditorInputChange}/>
+            <textarea className="form-control post-editor-input" value={this.state.newPostBody} onChange={this.handlePostEditorInputChange}/>
             <button className="btn btn-success post-editor-button" onClick={this.addPost}>Post</button>
           </div>
         </div>
